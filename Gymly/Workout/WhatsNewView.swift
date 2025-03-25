231921f1cccc2432d0266105211d73a93e27d391
Sebastian <sebastian.kucera@icloud.com>
2025-03-25 19:12:54 +0100

    UPDATE : Small changes

--- Gymly/Workout/WhatsNewView.swift
+++ Gymly/Workout/WhatsNewView.swift
@@ -13,15 +13,12 @@ struct WhatsNewView: View {
     var body: some View {
         NavigationView {
             List {
-                Section("New features in build \(WhatsNewManager.currentBuild)") {
-                    Label("New radar graph to show what muscle groups you exercise most you can find in user profile", systemImage: "speedometer")
-                    Label("See whats your BMI with new label in your user profile", systemImage: "heart")
-                    Label("Track how much weight did u gain or lost with new label in user profile", systemImage: "heart")
+                Section("New features") {
+                    Label("Updated UI for the settings", systemImage: "arrow.clockwise")
+                    Label("Added age and height labes in settings", systemImage: "arrow.clockwise")
                 }
                 Section("Fixes") {
-                    Label("Coping exercises fixed", systemImage: "hammer.fill")
-                    Label("Glitches and other bugs fixed", systemImage: "hammer.fill")
-                    Label("Importing splits via imessage fixed", systemImage: "hammer.fill")
+                    Label("No new bug fixes", systemImage: "hammer.fill")
                 }
                 Section("") {
                     Button("Got it!") {
@@ -31,7 +28,7 @@ struct WhatsNewView: View {
                 }
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            .navigationTitle("What's new?")
+            .navigationTitle("What's new in build \(WhatsNewManager.currentBuild)")
         }
     }
 }
